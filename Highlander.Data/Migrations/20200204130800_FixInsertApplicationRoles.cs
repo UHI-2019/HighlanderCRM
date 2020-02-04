932c52eef7fe5a996dd178554628ef17bb83dafc
CMChalmers <charlie.chalmersedu@gmail.com>
2020-02-04 14:25:58 +0000

    Add Roles Migration

---
+++ Highlander.Data/Migrations/20200204130800_FixInsertApplicationRoles.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Highlander.Data.Migrations
+{
+    public partial class FixInsertApplicationRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "051adf31-a303-4e5c-888c-377d2f947cb3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "40224790-5aa5-4d28-ace6-3fd10c8ead50");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "d9cea85a-f355-4fdc-a8e9-007841a94ae4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "505c3f1e-4b76-473d-b0c2-19329e301ae2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "d1cc4eb8-0640-4168-b76d-41fc442531ef");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "c6118110-6ac2-4765-aef6-3414ea911213");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ConcurrencyStamp",
+                value: "bb55349f-4f6a-4b8d-8e9e-adbd9f1d128c");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "b8671fec-e9d5-477c-b5e5-a56c3542f977");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "b0621cff-283e-4b3b-9ec2-65116d4e26a6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "5ac93523-d1f1-475d-bb43-f730230ed7e6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "dad62ed2-6bf0-4ca1-9c61-ad543e1c82f6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "64342c9a-3d08-4608-a55d-6f3055e16170");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "2a5412c8-6cec-4ebe-a013-75621c9ef6b4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ConcurrencyStamp",
+                value: "f2a98203-a7f3-450f-95ab-33bede6d788b");
+        }
+    }
+}
