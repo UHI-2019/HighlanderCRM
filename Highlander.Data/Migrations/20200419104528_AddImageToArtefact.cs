c89ff15ea73e3f6bbb6ee73b8d355bc3cf79232a
Charlie Chalmers <charlie.chalmers@toolsoup.com>
2020-04-21 09:24:32 +0100

    Basic Image Upload Added

---
+++ Highlander.Data/Migrations/20200419104528_AddImageToArtefact.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Highlander.Data.Migrations
+{
+    public partial class AddImageToArtefact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Artefacts",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "990c620c-32bf-416b-b5cb-bfbdc0d0ed4c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "49351aeb-2fb3-47ba-b95f-4c2e5bf498d6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "53e83d1c-435b-4984-bba1-28f6008acb78");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "5297aa8b-b66c-42ed-ad01-a4f4d2d05baa");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "e6b0375a-5310-4c51-a19d-2bbf6edf53fa");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "c77e458c-e46f-4b4e-a6ad-59360a1a3933");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ConcurrencyStamp",
+                value: "9b93a5a2-1e7b-47aa-810a-9d885b14650d");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Artefacts");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "16828b04-18b4-4479-b518-38ee765561ec");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "eefdc4c8-1101-4cfa-b167-b3156ed43971");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "6ecc9767-585b-4bf1-974d-c03d20b9f4b0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "d1b7e8e3-09df-4c73-b2cb-86c8ba2eceb4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "d5a260ec-e9b5-4883-ac2d-15a96f517310");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "8351694b-09ec-44ef-a2d2-02cd4f8e1ecb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ConcurrencyStamp",
+                value: "9d5c4678-267d-45e9-b1df-9fc80bded073");
+        }
+    }
+}
---
+++ Highlander.Web/Helpers/FileUploadHelper.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Highlander.Web.Helpers
+{
+    public class FileUploadHelper
+    {
+        public string ConnectionString { get; } // add option to override
+
+        private readonly IConfiguration _configuration;
+        private readonly CloudBlobClient _client;
+        private readonly CloudBlobContainer _container;
+
+        public FileUploadHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = configuration.GetConnectionString("StorageConnection");
+
+            var account = CloudStorageAccount.Parse(ConnectionString);
+            _client = account.CreateCloudBlobClient();
+            _container = _client.GetContainerReference("highlander");
+        }
+
+        public byte[] ConvertImageToByteArray(IFormFile image)
+        {
+            byte[] result = null;
+
+            using (var fileStream = image.OpenReadStream())
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    fileStream.CopyTo(memoryStream);
+                    result = memoryStream.ToArray();
+                }
+                return result;
+            }
+        }
+
+        public async Task<string> UploadImageByteArray(byte[] imageBytes, string fileName, string contentType)
+        {
+            if (imageBytes == null || imageBytes.Length == 0)
+            {
+                return null;
+            }
+
+            var cloudBlockBlob = _container.GetBlockBlobReference(fileName);
+            cloudBlockBlob.Properties.ContentType = contentType;
+
+            const int byteArrayStartIndex = 0;
+
+            await cloudBlockBlob.UploadFromByteArrayAsync(
+                imageBytes,
+                byteArrayStartIndex,
+                imageBytes.Length);
+
+            var imageFullUrlPath = cloudBlockBlob.Uri.ToString();
+
+            return imageFullUrlPath;
+        }
+    }
+}
