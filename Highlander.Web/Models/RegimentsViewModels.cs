03d0725e81447d0903f80b70b61280d6b37cb695
Charlie Chalmers <charlie.chalmers@toolsoup.com>
2020-04-28 13:50:15 +0100

    Add Regiments CRUD

---
+++ Highlander.Web/Models/RegimentsViewModels.cs
@@ -0,0 +1,13 @@
+﻿using Highlander.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Highlander.Web.Models
+{
+    public class RegimentsIndexViewModel
+    {
+        public IEnumerable<Regiment> Regiments { get; set; }
+    }
+}
